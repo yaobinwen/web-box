37bea9915009b1ca346cd2c224f9fee11a9d1824
yaobinwen <robin.wyb@gmail.com>
2018-07-10 09:09:01 -0400

    Add more comments

--- topics/test-async-component/code/src/components/__test__/About.test.js
+++ topics/test-async-component/code/src/components/__test__/About.test.js
@@ -9,29 +9,29 @@ enzyme.configure({ adapter: new Adapter() });
 // NOTE(ywen): We reuse the 'fetchMock' in 'About.js'. In real test code, you
 // need to define your own fetch mocks.
 
-describe('Smoke Testing', () => {
-  test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<About />, div);
-    // ReactDOM.unmountComponentAtNode(div);
-  });
-})
-
 describe('Functionality Testing', () => {
   test('Test a single async call', (done) => {
+    // Make sure the expects are called.
     expect.assertions(1)
 
     let wrapper = mount(
       <About />, document.createElement('div')
     );
 
-    // NOTE(ywen): Assumptions:
-    // 1). The 'setState' is not called before this function returns.
+    // NOTE(ywen): Because the `updateAuthor` is called in the resolver of the
+    // fetch promise, it is thus called after this test function returns. We
+    // can replace the method with the mocked one after mounting and it will
+    // still be called.
     let appInst = wrapper.instance();
     let old = appInst.updateAuthor;
     appInst.updateAuthor = jest.fn(
       (name) => {
+        // Call the old method first.
         old.call(appInst, name)
+        // Call expect statement to test the expectation.
+        // TODO(ywen): I use a timer to make sure the expect and done are
+        // scheduled to be called after state update is actually done. But I
+        // need to double check if this is true.
         setTimeout(() => {
           expect(wrapper.find('p').at(0).text()).toEqual('Author: Yaobin Wen')
           done()
@@ -52,15 +52,15 @@ describe('Functionality Testing', () => {
       expect(wrapper.find('p').at(1).text()).toEqual('Description: Test async component')
     }
 
-    // NOTE(ywen): Assumptions:
-    // 1). The 'setState' is not called before this function returns.
     let doneCounter = 0
     let updateDone = () => {
+      // Only call the expects and done when both updates are done.
       if (++doneCounter === 2) {
         expects()
         done()
       }
     }
+
     let appInst = wrapper.instance();
     let oldUpdateAuthor = appInst.updateAuthor;
     let oldUpdateDescription = appInst.updateDescription;
@@ -91,13 +91,20 @@ describe('Functionality Testing', () => {
     // Replace the class method with the mocked version.
     let callCounter = 0;
     let oldSetState = About.prototype.setState;
-    About.prototype.setState = jest.fn(function(changes) {
-      oldSetState.call(this, changes);
-      if (++callCounter === 2) {
-        expects(this);
-        done();
+    About.prototype.setState = jest.fn(
+      // NOTE(ywen): We must not use arrow function to define the mocked
+      // implementation because the arrow function uses the `this` at the time
+      // of definition rather than run-time. If we want the `oldSetState` to be
+      // called with the context at the run-time, we need to use `function` to
+      // define the mocked implementation.
+      function(changes) {
+        oldSetState.call(this, changes);
+        if (++callCounter === 2) {
+          expects(this);
+          done();
+        }
       }
-    });
+    );
 
     // Create the tested component.
     let wrapper = mount(<About />, document.createElement('div'));
--- topics/test-async-component/code/src/components/__test__/License.test.js
+++ topics/test-async-component/code/src/components/__test__/License.test.js
@@ -9,18 +9,14 @@ enzyme.configure({ adapter: new Adapter() });
 // NOTE(ywen): We reuse the 'fetchMock' in 'License.js'. In real test code, you
 // need to define your own fetch mocks.
 
-describe('Smoke Testing', () => {
-  test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<License />, div);
-    // ReactDOM.unmountComponentAtNode(div);
-  });
-})
-
 describe('Functionality Testing', () => {
   test('Test a single async call', (done) => {
     expect.assertions(1)
 
+    // NOTE(ywen): To use promise for testing, the fetching method
+    // (`fetchLicense` in this case) must return the promise. If the product
+    // code doesn't return the promise, you may need to use the other testing
+    // methods as shown in `About.test.js` to test it.
     let promise;
     let old = License.prototype.fetchLicense;
     License.prototype.fetchLicense = jest.fn(function() {
