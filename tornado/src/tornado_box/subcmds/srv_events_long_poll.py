0476e1591bfae027dadaf6917cb1bdb36f7a0643
Yaobin Wen <robin.wyb@gmail.com>
2024-04-18 19:55:35 -0400

    Implement a timeout for `srv_events_long_poll`'s `/events/poll` endpoint

--- tornado/src/tornado_box/subcmds/srv_events_long_poll.py
+++ tornado/src/tornado_box/subcmds/srv_events_long_poll.py
@@ -1,64 +1,137 @@
 import asyncio
-import enum
-import json
+import concurrent.futures
 import logging
 import pathlib
 import random
 import tornado.web
 
+from google.protobuf import json_format
 from tornado_box.app_base import AppBase
 from tornado_box.handler_base import HandlerBase
+from tornado_box.v0.events_long_poll.event_pb2 import Event as EventExternal
+from tornado_box.v0.events_long_poll.poll_pb2 import (
+    EC_SUCCESS,
+    EC_TOO_MANY_TIMEOUTS,
+    EC_TIMEOUT_NOT_NUMERIC,
+    EC_TIMEOUT,
+    PollResponse,
+)
 
 
 logger = logging.getLogger(pathlib.Path(__file__).name)
 
 
-class EventsGetHandler(HandlerBase):
+class EventInternal(object):
+    def __init__(self, num=None):
+        self._num = num
+
+    @property
+    def num(self):
+        return self._num
+
+
+class EventsLongPollHandlerBase(HandlerBase):
+    def _add_headers(self):
+        self.add_header("Content-Type", "application/json")
+        self.allow_origin_if_needed()
+
+
+class EventsGetHandler(EventsLongPollHandlerBase):
     def initialize(self, *, app):
         self.app = app
 
     def get(self, *args, **kwargs):
         logger.info("getting current events...")
 
-        events = json.dumps(self.app.events)
+        # Convert EventInternal instances to EventExternal instances.
+        events = [EventExternal(num=e.num) for e in self.app.events]
         self.app.events = []
 
         self.set_status(200)
-        self.add_header("Content-Type", "application/json")
-        self.allow_origin_if_needed()
-        self.write(events)
+        self._add_headers()
+        self.write(
+            json_format.MessageToJson(
+                PollResponse(code=EC_SUCCESS, events=events),
+                including_default_value_fields=True,
+                preserving_proto_field_name=True,
+            )
+        )
         self.finish()
 
 
-class EventsPollHandler(HandlerBase):
+class EventsPollHandler(EventsLongPollHandlerBase):
     def initialize(self, *, app):
         self.app = app
 
+    def _request_error(self, http_code, error_code):
+        self.set_status(http_code)
+        self._add_headers()
+        self.write(
+            json_format.MessageToJson(
+                PollResponse(code=error_code, events=[]),
+                including_default_value_fields=True,
+                preserving_proto_field_name=True,
+            )
+        )
+        self.finish()
+
+    def _bad_request(self, error_code):
+        self._request_error(http_code=400, error_code=error_code)
+
+    def _request_timeout(self):
+        self._request_error(http_code=408, error_code=EC_TIMEOUT)
+
     async def get(self, *args, **kwargs):
-        logger.info("polling for events (handler: %s)...", id(self))
+        # By default, the request doesn't time out and will keep polling
+        # forever.
+        timeout_s = None
+
+        timeout_list = self.request.query_arguments.get("timeout-in-s", None)
+        if timeout_list:
+            if len(timeout_list) > 1:
+                return self._bad_request(error_code=EC_TOO_MANY_TIMEOUTS)
+            else:
+                try:
+                    timeout_s = float(timeout_list[0])
+                except ValueError:
+                    return self._bad_request(error_code=EC_TIMEOUT_NOT_NUMERIC)
+
+        logger.info(
+            "polling for events (handler: %s; timeout: %s)...",
+            id(self),
+            timeout_s if timeout_s else "infinity",
+        )
 
         if not self.app.events:
-            await self.app.wait_for_events()
-            logger.info("event occurred")
+            try:
+                await self.app.wait_for_events(timeout_s=timeout_s)
+                logger.info("event occurred")
+            except concurrent.futures.TimeoutError:
+                return self._request_timeout()
 
-        events = json.dumps(self.app.events)
+        events = [EventExternal(num=e.num) for e in self.app.events]
         self.app.events = []
 
         self.set_status(200)
-        self.add_header("Content-Type", "application/json")
-        self.allow_origin_if_needed()
-        self.write(events)
+        self._add_headers()
+        self.write(
+            json_format.MessageToJson(
+                PollResponse(code=EC_SUCCESS, events=events),
+                including_default_value_fields=True,
+                preserving_proto_field_name=True,
+            )
+        )
         self.finish()
 
 
-class EventsTriggerHandler(HandlerBase):
+class EventsTriggerHandler(EventsLongPollHandlerBase):
     def initialize(self, *, app):
         self.app = app
 
     def post(self, *args, **kwargs):
-        event = int(self.request.body.decode("utf-8"))
+        event = json_format.Parse(self.request.body, EventExternal())
 
-        self.app._trigger_event(event=event)
+        self.app._trigger_event(num=event.num)
 
         self.set_status(200)
         self.allow_origin_if_needed()
@@ -135,25 +208,26 @@ class App(AppBase):
     def _generate_event(self):
         generate = random.choice([True, False])
         if generate:
-            self._trigger_event(event=self.event_number)
+            self._trigger_event(num=self.event_number)
             self.event_number += 1
 
-    def _trigger_event(self, event):
+    def _trigger_event(self, num):
         if self.generating_event:
             return
 
         self.generating_event = True
 
-        logger.debug("triggering an event '%s'...", event)
-        self.events.append(event)
+        logger.debug("triggering an event '%s'...", num)
+        self.events.append(EventInternal(num=num))
 
         self.event_occurred.set()
         self.event_occurred.clear()
 
         self.generating_event = False
 
-    async def wait_for_events(self):
-        await self.event_occurred.wait()
+    async def wait_for_events(self, timeout_s=None):
+        future = asyncio.ensure_future(self.event_occurred.wait())
+        await asyncio.wait_for(future, timeout=timeout_s)
 
 
 def subcmd_srv_events_long_poll(
