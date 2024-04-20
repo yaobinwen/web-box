3a33d11479f5785d43e8e296703e2acd1a48a0a7
Yaobin Wen <robin.wyb@gmail.com>
2024-04-19 22:05:54 -0400

    nodejs: Make `events-long-poll.js` more flexible to use

--- nodejs/events-long-poll.js
+++ nodejs/events-long-poll.js
@@ -7,24 +7,28 @@ import { Command } from "commander";
 const syntax = new Command();
 syntax
   .usage("[OPTIONS]")
-  .option("--api-root <URL>", "The root HTTP endpoint", "http://localhost")
+  .option(
+    "--api-root <URL>",
+    "The root HTTP endpoint",
+    "http://localhost:20361"
+  )
+  .option("--timeout-s <DURATION>", "The polling timeout in seconds", 1)
+  .option("--poll-max-count <INTEGER>", "The number of times to poll events", 3)
   .parse(process.argv);
 
 const options = syntax.opts();
+console.debug("Options: ", options);
 
 let pollCounter = 0;
-let pollTimeout = null;
+const POLL_MAX_COUNT = options.pollMaxCount;
+const URL = `${options.apiRoot}/events/poll?timeout-in-s=${options.timeoutS}`;
+const HEADERS = { Accept: "application/json" };
 const pollEvents = async () => {
   pollCounter += 1;
-  console.debug(`polling events (count: ${pollCounter})...`);
-
-  const url = `${options.apiRoot}/tornado-box/events/poll?timeout-in-s=3`;
-  const h = {
-    Accept: "application/json",
-  };
 
   const _pollEventsOnce = async () => {
-    const response = await axios.get(url, { headers: h });
+    console.debug(`polling events (count: ${pollCounter}) at ${URL}...`);
+    const response = await axios.get(URL, { headers: HEADERS });
     return response;
   };
 
@@ -34,8 +38,8 @@ const pollEvents = async () => {
   } catch (error) {
     console.error(`${error.response.status} (${error.response.statusText})`);
   } finally {
-    if (pollCounter < 10) {
-      pollTimeout = setTimeout(pollEvents, 0);
+    if (pollCounter < POLL_MAX_COUNT) {
+      setTimeout(pollEvents, 0);
     }
   }
 };
