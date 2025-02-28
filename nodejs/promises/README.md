debace640019a4f77706227f47bcba1be33eaf5b
Yaobin Wen <robin.wyb@gmail.com>
2025-02-28 10:57:11 -0500

    `nodejs`: Add demo code for Promises

---
+++ nodejs/promises/README.md
@@ -0,0 +1,12 @@
+# Promises: Please promise me to make Promises easier to comprehend in the future JavaScript versions
+
+Somehow I just can't fully grasp the correct use of JavaScript promises. The code in this directory is my notes of learning how to use them.
+
+Here are some references:
+- [1] [Promises/A+](https://promisesaplus.com/)
+- [2] [MDN: Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
+- [3] [javascript.info: Promises, async/await](https://javascript.info/async)
+
+How to run the code:
+- Install `node.js` v14.18.0+.
+- Run `nodejs <filename>`. For example: `nodejs executor-no-params.js`.
---
+++ nodejs/promises/unhandled-rejection.js
@@ -0,0 +1,83 @@
+#!/usr/bin/nodejs
+
+/**
+ * The `catch` of a promise chain only catches the errors that are thrown in
+ * that chain. If a separate promise chain is started inside the outer chain,
+ * the thrown errors will not be caught by the outer chain and may result in
+ * the "unhandled rejection" error.
+ */
+
+async function c1_f1() {
+  console.log('chain 1: f1');
+}
+
+async function c1_f2() {
+  console.log('chain 1: f2');
+  throw new Error('error on chain 1 f2');
+}
+
+async function c1_f3() {
+  console.log('chain 1: f3');
+}
+
+console.log("=".repeat(20))
+console.log("Demo 1: Catching error thrown in the same promise chain")
+await c1_f1()
+  .then(() => c1_f2())
+  .then(() => c1_f3())
+  .catch(error => {
+    console.error('Aww! What happened?!', error);
+  });
+
+async function c2_f2() {
+  console.log('chain 2: f2')
+  throw new Error('error on chain 2 f2')
+}
+
+console.log("=".repeat(20))
+console.log("Demo 2a: Link `c2_f2()` to the outer promise chain by returning its result")
+await c1_f1()
+  .then(() => {
+    // By returning the result of `c2_f2()`, we link `c2_f2()` to the outer
+    // promise chain, so the error thrown out of `c2_f2()` can be caught by
+    // the `catch` block.
+    return c2_f2()
+  })
+  .then(() => c1_f3())
+  .catch(error => {
+    console.error('Aww! What happened?!', error);
+  });
+
+console.log("=".repeat(20))
+console.log("Demo 2b: Build a `catch` for the separate promise chain")
+await c1_f1()
+  .then(() => {
+    // By returning the result of `c2_f2()`, we link `c2_f2()` to the outer
+    // promise chain, so the error thrown out of `c2_f2()` can be caught by
+    // the `catch` block.
+    c2_f2()
+    .catch(error => {
+      // The error is caught and handled here so the outer promise chain would
+      // not be interrupted and can still move onto `c1_f3()`.
+      console.error('Caught an error: ', error)
+    })
+  })
+  .then(() => c1_f3())
+  .catch(error => {
+    console.error('Aww! What happened?!', error);
+  });
+
+console.log("=".repeat(20))
+console.log("Demo 3: Errors thrown in a separate promise chain can't be caught")
+await c1_f1()
+  .then(() => {
+    // Because the return value of `c2_f2()` was not returned to the outer
+    // promise chain, `c2_f2()` itself became a separate promise chain than
+    // the outer one, so the error that's thrown by `c2_f2()` can't be caught
+    // by the `catch` block for the outer promise chain.
+    c2_f2()
+  })
+  .then(() => c1_f3())
+  .catch(error => {
+    console.error('Aww! What happened?!', error);
+  });
